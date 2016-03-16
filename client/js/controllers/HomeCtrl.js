b68cf103a0e4061a765d6976681ea7fa22ad89d7
quicksam24 <s.bourgault@hotmail.ca>
2016-03-16 15:44:24 +0000

    lambda test

--- client/js/controllers/HomeCtrl.js
+++ client/js/controllers/HomeCtrl.js
@@ -4,6 +4,47 @@ angular.module('BarterApp').controller('HomeCtrl', ['$scope', function($scope) {
     
     $scope.message = "Message";
     
+    testLambda();
+    
+    function testLambda() {
+        
+        AWS.config.credentials.get(function(err) {
+           
+            if (err) {
+                console.log(err);
+            }
+            else {
+                
+                var lambda = new AWS.Lambda({region: 'us-west-2'});
+                
+                var params = {
+                    FunctionName: 'lambdaTestHandlerSam',
+                    Payload: JSON.stringify({
+                        val1: "val1",
+                        val2: "val2"
+                    }) 
+                };
+                
+                lambda.invoke(params, function(error, response) {
+                    if (error) {
+                        console.log(error);
+                    } 
+                    else {
+                        var payload = JSON.parse(response.Payload);
+                        if (payload.errorMessage) {
+                            console.log(payload.errorMessage, null);
+                        }
+                        else {
+                            console.log(null, payload);
+                        }
+                    }
+                });
+            }
+        });
+    }
+    
+
+    
 }]);
 
 
